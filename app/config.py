99a2afc290889b4b5052e3970e857a93c93a299e
mallah-elmehdi <elmehdimallah@gmail.com>
2022-06-17 15:36:49 +0100

    add configuration

---
+++ app/config.py
@@ -0,0 +1,22 @@
+from pydantic import BaseSettings
+from functools import lru_cache
+
+
+class Settings:
+    class SettingsClass(BaseSettings):
+        db: str
+        db_name: str
+        cookie_name: str
+        jwt_algorithm: str
+        jwt_secret: str
+        user_id: str
+        user_name: str
+        password: str
+
+        class Config:
+            env_file = ".env"
+            env_file_encoding = "utf-8"
+
+    @lru_cache()
+    def settings(self):
+        return self.SettingsClass()
